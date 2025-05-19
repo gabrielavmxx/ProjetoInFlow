ae30c4fde4c7e3d171cc434b4af7c352da89ecf1
felavor <fellipe.lavor@sptech.school>
2025-05-19 18:08:18 -0300

    Atualizando app.js (dash)

--- Site/app.js
+++ Site/app.js
@@ -22,7 +22,9 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
+var dashRouter = require('./src/routes/dashboard');
 var uploadUser = require('./src/middleware/uploadImage');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,6 +34,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
+app.use("/dashboard", dashRouter);
 
 app.post('/profile', uploadUser.single('imagem'), async (req, res ) => {
 
