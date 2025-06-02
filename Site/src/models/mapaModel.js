a61b8757708b5c02e7f0ef71a75a254af93f2bc9
leticia0510 <leticia.ssantos@sptech.school>
2025-06-02 09:58:38 -0300

    mapa de calor

--- Site/src/models/mapaModel.js
+++ Site/src/models/mapaModel.js
@@ -3,23 +3,18 @@ var database = require("../database/config");
 function buscarValoresDosUltimos5minPorSensor(idSupermercado) {
 
     var instrucaoSql = `
-    SELECT s.numero_serie, COUNT(presenca) AS presenca FROM registros r
+    SELECT s.x, s.y, COUNT(presenca) AS value FROM registros r
     INNER JOIN sensor s ON r.fksensor = s.id
     INNER JOIN corredor c ON s.fkcorredor = c.id
     INNER JOIN supermercado sm ON c.fksupermercado = sm.id
     INNER JOIN empresa em ON sm.fkempresa = em.id
     WHERE sm.id = ${idSupermercado} AND 
-    datahora > DATE_SUB(('2025-05-19 08:11:00'), INTERVAL 5 MINUTE)
-    GROUP BY s.numero_serie;
+    datahora > DATE_SUB(('2025-05-19 10:11:00'), INTERVAL 20 MINUTE)
+    GROUP BY s.x, s.y;
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
-}
-
-function somaDosValoresEncontrados() {
-
-    var instrucaoSql = ``
 }
 
 module.exports = {
