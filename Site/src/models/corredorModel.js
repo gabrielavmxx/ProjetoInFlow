724f281f09dcec8fd547c590d7d32685c2577e69
isabellachagas028 <isabella.chagas@sptech.school>
2025-06-04 14:40:16 -0300

    grafico corredores funcionando

--- Site/src/models/corredorModel.js
+++ Site/src/models/corredorModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config");
 
 function buscarCorredoresPorSupermercado(IdSupermercado) {
 
-  var instrucaoSql = `  select cor.id, ar.nome from corredor cor inner join supermercado sup on sup.id=cor.fksupermercado inner join areas ar on cor.fkarea=ar.id where fksupermercado = ${IdSupermercado}`;
+  var instrucaoSql = `  select cor.id, ar.nome,cor.posicao,cor.fksupermercado from corredor cor inner join supermercado sup on sup.id=cor.fksupermercado inner join areas ar on cor.fkarea=ar.id where fksupermercado = ${IdSupermercado}`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
