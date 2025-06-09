a3058a0120bb932df88f5c81ff35ac34e1bd0c9a
Hanieh24 <hanieh.ashouri@sptech.school>
2025-06-09 11:27:05 -0300

    o cadastro pegando o fksupermercado e mostra o resultado na pagina do admin

--- Site/src/controllers/usuarioController.js
+++ Site/src/controllers/usuarioController.js
@@ -66,6 +66,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var supermercado = req.body.supermercadoServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -77,7 +78,7 @@ function cadastrar(req, res) {
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, supermercado)
             .then(
                 function (resultado) {
                     res.json(resultado);
